55ceb66ee4a522396d1c7caf8e84725533173b22
Kacper <kacperkapitrebacz@gmail.com>
2023-06-14 20:44:03 +0200

    App generate the python code

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,85 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 4 {
-        eprintln!("Sposób użycia: {} FROM TO FILE", args[0]);
-        eprintln!("Sposób użycia: {} even_odd_python -20000 20000", args[0]);
+        eprintln!("Usage: {} FROM TO FILE", args[0]);
+        eprintln!("Example: {} even_odd_python -20000 20000", args[0]);
         std::process::exit(1);
     }
 
-    let firstNumber = &args[2].parse::<i64>();
-    let lastNumber = &args[3].parse::<i64>();
-    let fileName = &args[1].parse::<String>();
+    let first_number = match args[2].parse::<i64>() {
+        Ok(number) => number,
+        Err(_) => {
+            eprintln!("ERROR! -> Invalid value for FROM");
+            std::process::exit(1);
+        }
+    };
 
-    match firstNumber {
-        Ok(_) => println!("Correctly stated value FROM"),
+    let last_number = match args[3].parse::<i64>() {
+        Ok(number) => number,
         Err(_) => {
-            eprintln!("Uncorrectly stated value FROM");
+            eprintln!("ERROR! -> Invalid value for TO");
             std::process::exit(1);
-        },
+        }
+    };
+
+    let file_name = &args[1];
+
+    let file = match File::create(format!("{}.py", file_name)) {
+        Ok(file) => file,
+        Err(_) => {
+            eprintln!("ERROR! -> Failed to create file");
+            std::process::exit(1);
+        }
+    };
+
+    // FIRST LINES FOR PYTHON CODE
+    // INPUT AND CODE SECURITY
+    writeln!(&file, "while True:").expect("Error writing to file");
+    writeln!(&file, "   try:").expect("Error writing to file");
+    writeln!(&file, "      input_number = int(input('Enter a number to check: '))").expect("Error writing to file");
+    writeln!(&file, "   except ValueError:").expect("Error writing to file");
+    writeln!(&file, "      print('\\tERROR! -> WRONG INPUT. ENTER AGAIN\\n')").expect("Error writing to file");
+    writeln!(&file, "");  // empty line
+
+    for i in first_number..=last_number {
+        if i == first_number {
+            writeln!(&file, "   if input_number == {}:", &i)
+                .expect("Error writing to file");
+
+            if i % 2 == 0 {
+                writeln!(&file, "       print('Even')")
+                    .expect("Error writing to file");
+            } else {
+                writeln!(&file, "       print('Odd')")
+                    .expect("Error writing to file");
+            }
+
+            writeln!(&file, "")
+                .expect("Error writing to file");
+        } else {
+            writeln!(&file, "   elif input_number == {}:", &i)
+                .expect("Error writing to file");
+
+            if i % 2 == 0 {
+                writeln!(&file, "       print('Even')")
+                    .expect("Error writing to file");
+            } else {
+                writeln!(&file, "       print('Odd')")
+                    .expect("Error writing to file");
+            }
+
+            writeln!(&file, "") // empty line
+                .expect("Error writing to file");
+        }
     }
 
-    match lastNumber {
-        Ok(_) => println!("Correctly stated value TO"),
-        Err(_) => {
-            eprintln!("Uncorrectly stated value TO");
-            std::process::exit(1);
-        },
-    }
-    
-    match fileName {
-        Ok(_) => println!("Correctly stated value NAME"),
-        Err(_) => {
-            eprintln!("Uncorrectly stated value NAME");
-            std::process::exit(1);
-        },
-    }
+    writeln!(&file, "   else:")
+        .expect("Error writing to file");
+    writeln!(&file, "       print('This number is out of range')")
+        .expect("Error writing to file");
 }
